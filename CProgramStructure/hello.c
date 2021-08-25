ac786e788ff47ec195da3e3891bf1a1934f2e1ff
Esteban <juangprado333@gmail.com>
2021-08-25 16:41:34 -0500

    Inicio Curso de introducción a C

---
+++ CProgramStructure/hello.c
@@ -0,0 +1,14 @@
+ /*Preprocessors Comands, Zona donde incluimos nuestras librerias.*/
+#include <stdio.h>
+
+/*función principal, en donde la ejecución de programa comienza*/
+int main()
+
+{
+
+    /*  Así es como se comenta en C 
+    first hello world in C language
+    */
+    printf("Hello platzi");
+    return 0;
+}
