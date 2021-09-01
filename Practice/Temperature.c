d082ca1b659a0d47afa88d7273b449a4d4306338
Esteban <juangprado333@gmail.com>
2021-09-01 15:46:18 -0500

    Tercera versión ejercicio Temperature.c - Tabla Grados Celsius a Fahrenheit + Encabezado

--- Practice/Temperature.c
+++ Practice/Temperature.c
@@ -2,39 +2,38 @@
 
 int main()
 {
-    /*En C se deben declarar todas las variables antes de su uso*/
-    double fahr, celsius;
+    /*
+    En C se deben declarar todas las variables antes de su uso
+    */
+    int fahr, celsius;
     int lower, upper, step;
-    char cen, fhr;
+    char header [60] = "Equivalencias Grados \n\tCelsius a Fahrenheit\n\n\t °C\t    °F\n";
 
     lower = 0;
     upper = 300;
     step = 20;
 
-    cen = 'C';
-    fhr = 'F';
+    printf("\t%s\n", header);/* \t represeta la tabulación, \n representa un salto de línea*/
 
-    fahr = lower;
-    printf("%2c\t%6c\n", fhr,cen );/* \t represeta la tabulación*/
-
-    while (fahr <= upper)
+    celsius = lower;
+    
+    while (celsius<= upper)
     {
-        celsius = (fahr - 32.0)/ 1.8;
-
+        fahr = (celsius * 9/5) + 32;
         /*
             - %d Especifica que en esa posición va un tipo de dato entero.
             
             - %f Especifica que en esa posición va un tipo de dato con punto flotante.
 
-            - Los números 4 y 8 son la amplitud es decir que los números de 
-            la primera línea se van a imprimir en un campo de cuatro dígitos de 
-            ancho y los números de la segunda línea en un campo de ocho dígitos.
+            - Los números 3 y 6 son la amplitud es decir que los números de 
+            la primera línea se van a imprimir en un campo de tres dígitos de 
+            ancho y los números de la segunda línea en un campo de seis dígitos.
 
-            - % .1f específica que el número a imprimir tengo solo un dígito
+            - % .nf específica que el número a imprimir tengo una n>=0 cantidad de dígitos
             después del punto decimal.
         */
-        printf("%4.1f\t%8.1f\n", fahr, celsius);
-        fahr = fahr + step;
+        printf("\t%3d\t%6d\n", celsius, fahr);
+        celsius = celsius + step;
     }
 
     return 0;
