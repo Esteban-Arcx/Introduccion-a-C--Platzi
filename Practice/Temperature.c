0b39f4fdcebcd40497b28a19fe81af67fdbb935a
Esteban <juangprado333@gmail.com>
2021-09-01 11:59:52 -0500

    Segunda versión ejercicio Temperature.c - Tabla Grados Fahrenheit a celsius

--- Practice/Temperature.c
+++ Practice/Temperature.c
@@ -3,20 +3,37 @@
 int main()
 {
     /*En C se deben declarar todas las variables antes de su uso*/
-    int fahr, celsius;
+    double fahr, celsius;
     int lower, upper, step;
+    char cen, fhr;
 
     lower = 0;
     upper = 300;
     step = 20;
 
+    cen = 'C';
+    fhr = 'F';
+
     fahr = lower;
-    printf("C\tF\n");/* \t represeta la tabulación*/
+    printf("%2c\t%6c\n", fhr,cen );/* \t represeta la tabulación*/
 
     while (fahr <= upper)
     {
-        celsius = 5 *(fahr-32)/9;
-        printf("%d\t%d\n", fahr, celsius);/* %d Especifica que en esa posición va un tipo de dato entero*/
+        celsius = (fahr - 32.0)/ 1.8;
+
+        /*
+            - %d Especifica que en esa posición va un tipo de dato entero.
+            
+            - %f Especifica que en esa posición va un tipo de dato con punto flotante.
+
+            - Los números 4 y 8 son la amplitud es decir que los números de 
+            la primera línea se van a imprimir en un campo de cuatro dígitos de 
+            ancho y los números de la segunda línea en un campo de ocho dígitos.
+
+            - % .1f específica que el número a imprimir tengo solo un dígito
+            después del punto decimal.
+        */
+        printf("%4.1f\t%8.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 
